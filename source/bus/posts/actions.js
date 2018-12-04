0a3df27329782a57e537d054bc89d64aa9c466e5
Pixel <taraspixel@gmail.com>
2018-12-04 16:50:58 +0200

    redux-online-lesson-3

--- source/bus/posts/actions.js
+++ source/bus/posts/actions.js
@@ -1,5 +1,5 @@
 // Types
-import { FETCH_POSTS_ASYNC, FILL_POSTS } from './types';
+import { CREATE_POST, CREATE_POST_ASYNC, FETCH_POSTS_ASYNC, FILL_POSTS } from './types';
 
 // Instruments
 import { api } from '../../REST';
@@ -11,16 +11,31 @@ export const fillPosts = (posts) => {
     };
 };
 
-export const fetchPostsAsync = () => async (dispatch, getState) => {
+export const createPost = (post) => {
+    return {
+        type:    CREATE_POST,
+        payload: post,
+    };
+};
+
+export const fetchPostsAsync = () => async (dispatch) => {
     dispatch({
         type: FETCH_POSTS_ASYNC,
     });
-
-    console.log('-> getState()', getState());
 
     const response = await api.posts.fetch();
     const result = await response.json();
 
     dispatch(fillPosts(result.data));
+};
 
+export const createPostAsync = (comment) => async (dispatch) => {
+    dispatch({
+        type: CREATE_POST_ASYNC,
+    });
+
+    const response = await api.posts.create(comment);
+    const result = await response.json();
+
+    dispatch(createPost(result.data));
 };
